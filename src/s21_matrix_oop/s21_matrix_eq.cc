a0af018447b3b99c23ee824061e8bc01be5b5c0a
Egor <elin4cod@gmail.com>
2024-07-16 05:00:13 +0700

    eq done

---
+++ src/s21_matrix_oop/s21_matrix_eq.cc
@@ -0,0 +1,18 @@
+#include "s21_matrix_oop.h"
+
+bool S21Matrix::EqMatrix(const S21Matrix& other) {
+	bool result = true;
+	if (this->rows_ != other.rows_ || this->cols_ != other.cols_) {
+		result = false;
+	}
+	for (int i = 0; i < this->rows_ && result == true; i++) {
+		for (int j = 0; j < this->cols_ && result == true; j++) {
+			result = GetDiff(this->matrix_[i][j], other.matrix_[i][j]);
+		}
+	}
+	return result;
+}
+
+inline bool S21Matrix::GetDiff(double value1, double value2) {
+	return std::abs(value1 - value2) < S21Matrix::EPS;
+}
